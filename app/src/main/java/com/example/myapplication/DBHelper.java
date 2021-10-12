0a4a28474a2dae246550085693ef66b829408fd8
danilka.ezhov.03@mail.ru <danilka.ezhov.03@mail.ru>
2021-10-12 22:29:26 +0300

    gotovo

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -5,13 +5,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper  extends SQLiteOpenHelper{
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final String TABLE_AVTO = "avtomobil";
 
     public static final String KEY_ID = "_id";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_MARKA = "marka";
+    public static final String KEY_KYZOV = "kyzov";
+    public static final String KEY_GOD = "god";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,14 +20,14 @@ public class DBHelper  extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+        db.execSQL("create table " + TABLE_AVTO + "(" + KEY_ID
+                + " integer primary key," + KEY_MARKA + " text," + KEY_KYZOV + " text," +  KEY_GOD + " text" + ")");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        db.execSQL("drop table if exists " + TABLE_AVTO);
 
         onCreate(db);
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,30 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAdd, btnRead, btnClear;
-    EditText etName, etEmail;
-    TextView Output;
-
+    EditText etMarka, etKyzov, etGod;
     DBHelper dbHelper;
+    ContentValues contentValues;
+    SQLiteDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,67 +34,150 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.Add);
         btnAdd.setOnClickListener(this);
 
-        Output = (TextView) findViewById(R.id.output);
-
-        btnRead = (Button) findViewById(R.id.Read);
-        btnRead.setOnClickListener(this);
-
         btnClear = (Button) findViewById(R.id.Clear);
         btnClear.setOnClickListener(this);
 
-        etName = (EditText) findViewById(R.id.Name);
-        etEmail = (EditText) findViewById(R.id.Mail);
+        etMarka = (EditText) findViewById(R.id.Marka);
+        etKyzov = (EditText) findViewById(R.id.Kyzov);
+        etGod = (EditText) findViewById(R.id.God);
+
 
         dbHelper = new DBHelper(this);
+        database= dbHelper.getWritableDatabase();
+        UpdateTable();
+    }
+    public void UpdateTable() {
+        Cursor cursor = database.query(DBHelper.TABLE_AVTO, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int markaIndex = cursor.getColumnIndex(DBHelper.KEY_MARKA);
+            int kyzovIndex = cursor.getColumnIndex(DBHelper.KEY_KYZOV);
+            int godIndex = cursor.getColumnIndex(DBHelper.KEY_GOD);
+            TableLayout tb2 = findViewById(R.id.tableLayout2);
+            tb2.removeAllViews();
+            do {
+                TableRow tbOUT = new TableRow(this);
+                tbOUT.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+                TextView ID = new TextView(this);
+                params.weight = 1.0f;
+                ID.setLayoutParams(params);
+                ID.setText(cursor.getString(idIndex));
+                tbOUT.addView(ID);
+
+
+                TextView MARKA = new TextView(this);
+                params.weight = 1.0f;
+
+                MARKA.setLayoutParams(params);
+                MARKA.setText(cursor.getString(markaIndex));
+                tbOUT.addView(MARKA);
+
+
+                TextView KYZOV = new TextView(this);
+                params.weight = 3.0f;
+                KYZOV.setLayoutParams(params);
+                KYZOV.setText(cursor.getString(kyzovIndex));
+                tbOUT.addView(KYZOV);
+
+                TextView GOD = new TextView(this);
+                params.weight = 3.0f;
+                GOD.setLayoutParams(params);
+                GOD.setText(cursor.getString(godIndex));
+                tbOUT.addView(GOD);
+
+
+                Button DELETE = new Button(this);
+                DELETE.setOnClickListener(this);
+                params.weight = 1.0f;
+                DELETE.setLayoutParams(params);
+                DELETE.setText("Удалить запись");
+                DELETE.setId(cursor.getInt(idIndex));
+                tbOUT.addView(DELETE);
+
+                tb2.addView(tbOUT);
+
+
+            } while (cursor.moveToNext());
+
+        }
+        cursor.close();
     }
 
     @Override
     public void onClick(View v) {
-
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
+        dbHelper = new DBHelper(this);
+        String marka = etMarka.getText().toString();
+        String kyzov = etKyzov.getText().toString();
+        String god = etGod.getText().toString();
+        contentValues = new ContentValues();
 
         switch (v.getId()) {
 
             case R.id.Add:
-                contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_MAIL, email);
 
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                break;
 
-            case R.id.Read:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                contentValues.put(DBHelper.KEY_MARKA, marka);
+                contentValues.put(DBHelper.KEY_KYZOV, kyzov);
+                contentValues.put(DBHelper.KEY_GOD, god);
 
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    Output.setText("");
-                    do {
-                        Output.append("ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else {
-                    Log.d("mLog", "0 rows");
-                    Output.setText("Данных нет!");
-                }
-                cursor.close();
+                database.insert(DBHelper.TABLE_AVTO, null, contentValues);
+                UpdateTable();
+                etMarka.setText(null);
+                etKyzov.setText(null);
+                etGod.setText(null);
+
                 break;
 
             case R.id.Clear:
-                Output.setText("");
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                database.delete(DBHelper.TABLE_AVTO, null, null);
+                TableLayout dbOutput = findViewById(R.id.tableLayout2);
+                dbOutput.removeAllViews();
+                etMarka.setText(null);
+                etKyzov.setText(null);
+                etGod.setText(null);
+                UpdateTable();
                 break;
+            default:
+                View outBDRow = (View) v.getParent();
+                ViewGroup outBD = (ViewGroup)  outBDRow.getParent();
+                outBD.removeView(outBDRow);
+                outBD.invalidate();
+
+                database.delete(DBHelper.TABLE_AVTO,DBHelper.KEY_ID + " = ?", new String[]{String.valueOf((v.getId()))});
+                contentValues = new ContentValues();
+                Cursor cursorUPD = database.query(DBHelper.TABLE_AVTO, null, null, null, null, null, null);
+
+                if (cursorUPD.moveToFirst()) {
+                    int idIndex = cursorUPD.getColumnIndex(DBHelper.KEY_ID);
+                    int markaIndex = cursorUPD.getColumnIndex(DBHelper.KEY_MARKA);
+                    int kyzovIndex = cursorUPD.getColumnIndex(DBHelper.KEY_KYZOV);
+                    int godIndex = cursorUPD.getColumnIndex(DBHelper.KEY_GOD);
+                    int realID =1;
+                    do{
+                        if(cursorUPD.getInt(idIndex)>realID)
+                        {
+                            contentValues.put(DBHelper.KEY_ID,realID);
+                            contentValues.put(DBHelper.KEY_MARKA,cursorUPD.getString(markaIndex));
+                            contentValues.put(DBHelper.KEY_KYZOV,cursorUPD.getString(kyzovIndex));
+                            contentValues.put(DBHelper.KEY_GOD,cursorUPD.getString(godIndex));
+                            database.replace(DBHelper.TABLE_AVTO,null,contentValues);
+
+
+                        }
+                        realID++;
+                    }while (cursorUPD.moveToNext());
+                    if(cursorUPD.moveToLast() && v.getId()!=realID){
+                        database.delete(DBHelper.TABLE_AVTO,DBHelper.KEY_ID+ " = ?", new String[]{cursorUPD.getString(idIndex)});
+                    }
+                    UpdateTable();
+                }
+
+
+                break;
+
+
         }
         dbHelper.close();
     }
